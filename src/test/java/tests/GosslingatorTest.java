191e441dc36c88f5374b602f5a8bcb06fa0a0d3d
HanaChv <hachsafe@gmail.com>
2019-04-24 18:40:50 +0200

    lekcia 24.4. pridanie testu warning message pridanie testu title stranky pridanie testu TOO MANY RAYNS

--- src/test/java/tests/GosslingatorTest.java
+++ src/test/java/tests/GosslingatorTest.java
@@ -9,8 +9,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class GosslingatorTest {
 
     @Test
-    public void itShouldAddOneRyan (){
-        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+    public void itShouldAddOneRyan() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         //0.spustit prehliadac
         WebDriver driver = new ChromeDriver();
         //1.otvorit stranku
@@ -22,7 +22,9 @@ public class GosslingatorTest {
         driver.findElement(By.id("removeRyan")).click();
         driver.findElement(By.id("removeRyan")).click();
         //3.overit pocitanie Ryanov
-                Assert.assertEquals( "1", driver.findElement(By.id("ryanCounter")).getText());
+        Assert.assertEquals("1", driver.findElement(By.cssSelector("div.ryan-counter h2")).getText());
+        System.out.println("Number of ryans: " + driver.findElement(By.cssSelector("div.ryan-counter h2")).getText());
+        Assert.assertEquals("ryan", driver.findElement(By.cssSelector("div.ryan-counter h3")).getText());
         //4.zatvorit prehliadac
         driver.close();
         //5.ukoncit session
@@ -31,5 +33,110 @@ public class GosslingatorTest {
         //čo je to "driver"??
         //.get (tak ako v Python :-)
         //Test failed nemame nainstalovany spravny driver - moja verzia google chrome je 73.atd - stiahnut spravnu verziu
+    }
+
+    @Test
+    public void itShouldAddTwoRyans() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        //0.spustit prehliadac
+        WebDriver driver = new ChromeDriver();
+        //1.otvorit stranku
+        driver.get("http://localhost/gosslingator.php");
+        //2.kliknut na tlacidlo
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("removeRyan")).click();
+        //3.overit pocitanie Ryanov
+        Assert.assertEquals("2", driver.findElement(By.cssSelector("div.ryan-counter h2")).getText());
+        Assert.assertEquals("ryans", driver.findElement(By.cssSelector("div.ryan-counter h3")).getText());
+        //4.zatvorit prehliadac
+        driver.close();
+        //5.ukoncit session
+        driver.quit();
+    }
+
+    @Test
+    public void itShouldDisplayTitle() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        //0.spustit prehliadac
+        WebDriver driver = new ChromeDriver();
+        //1.otvorit stranku
+        driver.get("http://localhost/gosslingator.php");
+        System.out.println(driver.findElement(By.cssSelector("h1.ryan-title")).getText());
+        Assert.assertEquals("GOSLINGATE ME", driver.findElement(By.cssSelector("h1.ryan-title")).getText());
+        driver.close();
+        //5.ukoncit session
+        driver.quit();
+    }
+    @Test
+    public void itShouldGetTooManyRyans() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        //0.spustit prehliadac
+        WebDriver driver = new ChromeDriver();
+        //1.otvorit stranku
+        driver.get("http://localhost/gosslingator.php");
+        //2.kliknut na tlacidlo
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+
+           //3.overit pocitanie Ryanov
+        //Assert.assertEquals("", driver.findElement(By.cssSelector("div.tooManyRyans h1")).getText());
+        System.out.println(driver.findElement(By.cssSelector("h1.tooManyRayns")).getText());
+        System.out.println(driver.findElement(By.cssSelector("div.ryan-counter h2")).getText());
+        //driver.findElement()
+        //4.zatvorit prehliadac
+        driver.close();
+        //5.ukoncit session
+        driver.quit();
     }
 }
