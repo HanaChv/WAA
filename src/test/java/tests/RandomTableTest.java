9db178f1e23519ad9c5cbf574350488355f20423
HanaChv <hachsafe@gmail.com>
2019-05-13 19:50:26 +0200

    lekcia 13.5.2019

--- src/test/java/tests/RandomTableTest.java
+++ src/test/java/tests/RandomTableTest.java
@@ -1,18 +1,29 @@
 package tests;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class RandomTableTest {
+    WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        //spustit prehliadac
+        driver = new ChromeDriver();
+        //otvorit stranku
+        driver.get("http://localhost/tabulka.php");
+    }
+
     @Test
     public void itShouldPrintLastRow() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        //0.spustit prehliadac
-        WebDriver driver = new ChromeDriver();
-        //1.otvorit stranku
         driver.get("http://localhost/tabulka.php");
         //2.vypisat meno z predposledneho riadku
         System.out.println("Meno v poslednom riadku: " + driver.findElement(By.xpath("//table/tbody/tr[last()-1]/td[2]")).getText());
@@ -22,4 +33,26 @@ public class RandomTableTest {
         driver.quit();
     }
 
+    @Test
+    public void itShouldContainDataForEachRow() {
+        //nova premenna tableRows obsahuje jednotlive riadky tabulky
+        List<WebElement> tableRows = driver.findElements(By.cssSelector("table tbody tr"));
+        for (WebElement tableRow : tableRows) {
+            Assert.assertFalse(tableRow.getText().isEmpty());
+//        System.out.println(tableRow.getText());
+        }
+    }
+
+    @Test
+    public void itShouldContainNameForEachRow() {
+        List<WebElement> tableRows = driver.findElements(By.cssSelector("table tbody tr"));
+        for (WebElement tableRow : tableRows) {
+            tableRow.findElement(By.cssSelector("td.nth-child(2)"));
+//            tableRow.findElement(By.xpath("./td[2]"));
+            WebElement rowName = tableRow.findElement(By.xpath("./td[2]"));
+            Assert.assertFalse(rowName.getText().isEmpty());
+        }
+
+    }
 }
+
