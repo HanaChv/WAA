72244d5da718108873258f5d1057039549308163
HanaChv <hachsafe@gmail.com>
2019-05-20 19:18:03 +0200

    lekcia 20.5.2019

--- src/test/java/tests/FellowshipTest.java
+++ src/test/java/tests/FellowshipTest.java
@@ -1,6 +1,5 @@
 package tests;
 
-import com.google.common.collect.Lists;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -9,8 +8,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,7 +18,7 @@ public class FellowshipTest {
 
     @Before
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "chromedriver74.exe");
         //spustit prehliadac
         driver = new ChromeDriver();
         //otvorit stranku
@@ -40,8 +39,8 @@ public class FellowshipTest {
         System.out.println("wefwef");
 
         for (WebElement fellowElement : fellowElements) {
-//            System.out.println(fellowElement.findElement(By.cssSelector("ul.list-of-fellows li h1")).getText());
-            //            WebElement fellowName = fellowElement.findElement(By.cssSelector("ul.list-of-fellows li h1"));
+//System.out.println(fellowElement.findElement(By.cssSelector("ul.list-of-fellows li h1")).getText());
+//WebElement fellowName = fellowElement.findElement(By.cssSelector("ul.list-of-fellows li h1"));
             Assert.assertFalse(fellowElement.findElement(By.cssSelector("h1")).getText().isEmpty());
 
         }
@@ -54,12 +53,25 @@ public class FellowshipTest {
         for (WebElement fellowElement : fellowElements) {
 //            v ramci kazdej kachlicky si najdem meno a to ulozim do zoznamu mien
             fellowNames.add(fellowElement.findElement(By.cssSelector("h1")).getText());
-            }
+        }
         System.out.println(fellowNames);
 //        over, že list obsahuje Gandalf-a a Aragorn-a
         Assert.assertTrue(fellowNames.contains("Gandalf"));
         Assert.assertTrue(fellowNames.contains("Aragorn"));
-        Assert.assertTrue(fellowNames.contains("Frodo"));}
+        Assert.assertTrue(fellowNames.contains("Frodo"));
     }
 
+    private void selectPokemon(String pokemonToSelect) {
+//        tato metoda bude prijimat jeden vstupny parameter, ktory je string
+        WebElement pokemonSelect = driver.findElement(By.cssSelector("select"));
+        new Select(pokemonSelect).selectByVisibleText(pokemonToSelect);
+    }
+
+    private String getActualMessage() {
+        return driver.findElement(By.cssSelector("div.pokemon h3")).getText();
+    }
+
+    private String getExpectedMessage(String pokemonName) {
+        return String.format("I choose you %s !", pokemonName);
+    }
 }
