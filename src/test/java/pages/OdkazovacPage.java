f7912f8dc51f4a61c7beac69f74681ba19a2ef14
HanaChv <hachsafe@gmail.com>
2019-05-27 19:55:32 +0200

    27.5. - oprava podla Martinovych commitov

--- src/test/java/pages/OdkazovacPage.java
+++ src/test/java/pages/OdkazovacPage.java
@@ -1,18 +1,43 @@
 package pages;
 
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import static org.openqa.selenium.By.cssSelector;
 
 public class OdkazovacPage {
-    WebDriver driver;
 
-    private void submitNewNote() {
-        driver.findElement(cssSelector("button.btn-block")).click();
+    @FindBy(name = "title")
+    //anotacia
+    private WebElement titleInput;
+
+    @FindBy(name = "author")
+    private WebElement authorInput;
+
+    @FindBy(name = "message")
+    private WebElement messageInput;
+
+    @FindBy(css = "button.btn-block")
+    private WebElement submitButton;
+
+    private WebDriver pageDriver;
+
+    public OdkazovacPage(WebDriver driver) {
+        this.pageDriver = driver;
+        PageFactory.initElements(pageDriver, this);
     }
 
-    private void checkNoteDetail(String title, String author, String message) {
-        WebElement detail = driver.findElement()
+    private void checkNoteDetail(Note noteToCheck) {
+        WebElement detail = pageDriver.findElement(By.cssSelector("div.content"));
+        Assert.assertEquals(noteToCheck.getTitle(), detail.findElement(By.cssSelector("h4.title")).getText());
+        Assert.assertEquals(noteToCheck.getAuthor(), detail.findElement(By.cssSelector("h4.recepient")).getText());
+        Assert.assertEquals(noteToCheck.getMessege(), detail.findElement(By.cssSelector("p")).getText());
+                ))
+
+        );
     }
 }
