2daedb3a85f67dc11d8012686a06d07241d5a990
HanaChv <hachsafe@gmail.com>
2019-05-29 19:32:36 +0200

    opravy

---
+++ src/test/java/tests/WaitForIt.java
@@ -0,0 +1,17 @@
+package tests;
+
+import base.TestBase;
+import org.junit.Before;
+import org.junit.Test;
+
+public class WaitForIt extends TestBase {
+
+    @Before
+    public void openPage(){
+        driver.get(BASE_URL + "/waitforit.php");
+    }
+@Test
+    public void waitForValue() throws InterruptedException {
+        String
+}
+}
