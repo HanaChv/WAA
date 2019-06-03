586c53faa98cc4c688fa47fab2829c58ab318417
HanaChv <hachsafe@gmail.com>
2019-06-03 11:28:23 +0200

    oprava/kontrola podla "Furby-git"

--- src/test/java/tests/RegistrationTest.java
+++ src/test/java/tests/RegistrationTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import base.TestBase;
 import io.codearte.jfairy.Fairy;
 import io.codearte.jfairy.producer.person.Person;
 import org.junit.After;
@@ -10,46 +11,43 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.RegistrationPage;
 
-public class RegistrationTest {
+public class RegistrationTest extends TestBase {
 
-    WebDriver driver;
+    public static final String "brano"="brano";
 
     @Before
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        //spustit prehliadac
-        driver = new ChromeDriver();
-        //otvorit stranku
-        driver.get("http://localhost/registracia.php");
-    }
-
-    @After
-    public void tearDown() {
-        //zatvorit prehliadac
-        driver.close();
-        //ukoncit session
-        driver.quit();
+    public void openPage() {
+        driver.get("/registracia.php");
     }
 
     @Test
     public void itShouldRegisterValidUser() {
-        Fairy fairy = Fairy.create();
-        Person person = fairy.person();
+        String email = "brano@mojsej.sk";
+        String meno = "brano";
+        String priezvisko = "mojsej";
+        String heslo = "123456";
 
-        driver.findElement(By.name("email")).sendKeys(person.getEmail());
-        driver.findElement(By.name("name")).sendKeys(person.getFirstName());
-        driver.findElement(By.name("surname")).sendKeys(person.getLastName());
-        driver.findElement(By.name("password")).sendKeys(person.getPassword());
-        driver.findElement(By.name("password-repeat")).sendKeys(person.getPassword());
+        //zadam zakladne udaje
+        RegistrationPage registrationPage = new RegistrationPage(driver);
+        registrationPage.enterData(email, meno, priezvisko, heslo);
+//        Fairy fairy = Fairy.create();
+//        Person person = fairy.person();
+
+//        driver.findElement(By.name("email")).sendKeys(person.getEmail());
+//        driver.findElement(By.name("name")).sendKeys(person.getFirstName());
+//        driver.findElement(By.name("surname")).sendKeys(person.getLastName());
+//        driver.findElement(By.name("password")).sendKeys(person.getPassword());
+//        driver.findElement(By.name("password-repeat")).sendKeys(person.getPassword());
+
         driver.findElement(By.id("checkbox")).click();
         driver.findElement(By.cssSelector("button.btn-success")).click();
-//overit uspesnu hlasku
         Assert.assertTrue(driver.findElement(By.cssSelector("div.alert-success")).isDisplayed());
-       //Assert.assertFalse(driver.findElement(By.cssSelector("div.alert-success")).isDisplayed());
-        }
-@Test
-    public void itShouldDisplayErrorMessageWhenInputsAreEmpty(){
+    }
+
+    @Test
+    public void itShouldDisplayErrorMessageWhenInputsAreEmpty() {
         driver.findElement(By.name("robot")).click();
         driver.findElement(By.cssSelector("button.btn-success")).click();
         Assert.assertTrue(driver.findElement(By.cssSelector("div.alert-danger")).isDisplayed());
